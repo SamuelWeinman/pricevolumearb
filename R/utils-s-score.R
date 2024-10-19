f0a5991c472778659b540cba05a334df8f21aa60
Samuel Weinman <samuel.weinman@d-fine.com>
2024-10-19 10:42:11 +0100

    utils-s-score comments

--- R/utils-s-score.R
+++ R/utils-s-score.R
@@ -21,24 +21,67 @@ decompose <- function(returns, nr_pc, h, L) {
 }
 
 
-### ESTIMATE THE COEFFICIENTS OF THE UHRNBECK MODEL, FOR EACH STOCK SEPRATELY
-# MODELS: MODELS OF THE FORM (RETURNS ~ FACTORS)
-# FOLLOWS SAME ALGORITHM AS APPENDIX A OF Marco Avellaneda & Jeong-Hyun Lee (2010)
-# IF B CLOSE TOO ONE, THERE IS NO MEAN REVERSION
-# HENCE, REJECT MEAN REVERSION IF b > 1-b_sensitivity
+#' Estimate Key Coefficients For All Models
+#'
+#' This function applies the `estimateSingleCoefficientsNumber` function to all models
+#' in the list. After key coefficients are calculated for each model, results are 
+#' combined in a single data frame.
+#'
+#' @param models A list of linear models for each stock, as obtained from the `decompose` function.
+#' @param b_sensitivity A numeric value, the sensitivity for b parameter to declare model as mean reverting.
+#'
+#' @return A data frame where each row corresponds to the estimated key coefficients of each model: 
+#' 'k' for speed of mean reversion, 'm' for the long term mean, 'sigma_squared' for variance, 
+#' 'sigma_eq_squared' for equilibrium variance and 'is_mean_reverting' for the model's mean-reverting status.
+#'
+#' @examples
+#' #Example data
+#' ret <- matrix(rnorm(25), 5, 5)
+#' num_pc <- 2
+#' historical <- 4
+#' preceding <- 1
+#' sensitivity <- 0.1
+#' #Use the function
+#' models <- decompose(ret, num_pc, historical, preceding)
+#' estimateCoefficients(models, sensitivity)
+#' 
+#' @export
 estimateCoefficeients <- function(models, b_sensitivity) {
   coefficients <- lapply(1:length(models), function(i) {
-    return(estimateCoefficeientsNumberI(models, i, b_sensitivity))
+    return(estimateSingleCoefficeientsNumber(models, i, b_sensitivity))
   })
 
-  # UPON CALCULATING COEFFICIENTS FOR EACH MODEL, PUT IN DATA FRAME
-  coefficients <- ldply(coefficients) # DATA FRAME
+  coefficients <- ldply(coefficients)
   colnames(coefficients) <- c("k", "m", "sigma_squared", "sigma_eq_squared", "is_mean_reverting")
-  # RETURN
   return(coefficients)
 }
 
-estimateCoefficeientsNumberI <- function(models, i, b_sensitivity) {
+#' Estimates Mean Reverting Coefficients
+#'
+#' This function estimates the key coefficients in a mean reverting model. 
+#' A linear model is created first using cumulative sum of residuals as variables. 
+#' Then, values for speed of mean reversion (k), long term mean (m), variance (sigma squared),
+#' equilibrium variance (sigma_eq_squared) and mean-reverting indicator are calculated.
+#'
+#' @param models A list of linear models for each stock, as obtained from the `decompose` function.
+#' @param i An integer, the index of the specific model required from the list.
+#' @param b_sensitivity A numeric value, sensitivity for checking if model_b < 1 - b_sensitivity.
+#'
+#' @return A numeric vector of length 5 containing key model parameters.
+#'
+#' @examples
+#' #Example data
+#' ret <- matrix(rnorm(25), 5, 5)
+#' num_pc <- 2
+#' historical <- 4
+#' preceding <- 1
+#' sensitivity <- 0.1
+#' #Use the function
+#' models <- decompose(ret, num_pc, historical, preceding)
+#' estimateCoefficeientsNumberI(models, 1, sensitivity)
+#' 
+#' @export
+estimateSingleCoefficeientsNumber <- function(models, i, b_sensitivity) {
   x <- cumsum(models[[i]]$residuals)
   model <- lm(x[2:l] ~ x[1:(l - 1)])
   a <- as.numeric(model$coefficients[1])
@@ -56,10 +99,31 @@ estimateCoefficeientsNumberI <- function(models, i, b_sensitivity) {
 }
 
 
-### CALCULATE S-SCORE FOR ALL STOCKS AS IN Marco Avellaneda & Jeong-Hyun Lee (2010)
-### THAT IS, THE AMOUNT BY WHICH THE STOCKS HAVE HAD TOO MUCH RETURNS
-### IF MEAN-REVERSION == 0, WE GIVE s = 0 AS THERE IS NO MEAN REVERSION.
-# RETURNS MUST BE HISTORY ONLY!
+#' Calculate S-Score for Mean Reverting Stocks
+#'
+#' This function calculates the S-Score for each stock which is proven to be mean reverting.
+#' It first decomposes the returns and estimate key coefficients of each model. Based on 
+#' these coefficients and mean reverting condition an S-score is calculated.
+#'
+#' @param returns A numeric matrix, the returns data.
+#' @param nr_pc An integer, the number of eigen portfolios to extract.
+#' @param h An integer, the number of recent historical days to be used.
+#' @param l An integer, the number of preceding days used for the model.
+#' @param b_sensitivity A numeric value, sensitivity for checking if model b coefficient is less than 1 minus this value.
+#'
+#' @return A list containing:
+#' * `s`: The computed S-Score for each stock.
+#' * `is_mean_reverting`: A logical vector indicating whether each stock is mean reverting.
+#'
+#' @examples
+#' #Example data
+#' ret <- matrix(rnorm(25), 5, 5)
+#' num_pc <- 2
+#' historical <- 4
+#' preceding <- 1
+#' sensitivity <- 0.1
+#' #Use the function
+#' calculateSScore(ret, num_pc, historical, preceding, sensitivity)
 calculateSScore <- function(returns, nr_pc, h, l, b_sensitivity) {
   models <- decompose(
     returns = returns,
@@ -71,8 +135,8 @@ calculateSScore <- function(returns, nr_pc, h, l, b_sensitivity) {
   coefficients <- estimateCoefficeients(models, b_sensitivity = b_sensitivity)
 
   s <- numeric(nrow(Returns))
-  index <- coefficients$is_mean_reverting == 1 # mean reversion
-  s[index] <- -coefficients$m[index] / sqrt(coefficients$sigma_eq_squared[index]) # CORRESPONDING S-SCORE
+  index <- coefficients$is_mean_reverting == 1 
+  s[index] <- -coefficients$m[index] / sqrt(coefficients$sigma_eq_squared[index])
 
   return(list(
     s = s,
